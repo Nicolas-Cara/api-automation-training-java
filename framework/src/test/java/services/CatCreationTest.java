29d45734ed18addb66d0f9905a7a83ab85c2afa9
Nicolas-Cara <caramesnicolas@gmail.com>
2025-06-11 21:35:42 -0300

    Milestone 3

--- framework/src/test/java/services/CatCreationTest.java
+++ framework/src/test/java/services/CatCreationTest.java
@@ -1,7 +1,10 @@
 package services;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import models.requests.CatModel;
@@ -11,7 +14,8 @@ import models.responses.ResponseContainer;
 public class CatCreationTest extends CatServiceTest {
 
     @Test
-    @DisplayName("Crear un nuevo gato, retorna data esperada")
+    @Tag("Smoke")
+    @DisplayName("Crear un nuevo gato, retorna data esperada, solo casos buenos")
     void createNewCat() {
         CatModel model = defaultCat();
         ResponseContainer<CatResponse> response = service.addCat(model, null);
@@ -19,6 +23,7 @@ public class CatCreationTest extends CatServiceTest {
         CatResponse responseModel = response.getData();
 
         Assertions.assertNotNull(response.getData());
+
         Assertions.assertEquals(model.getName(), responseModel.getName());
         Assertions.assertEquals(model.getAge(), responseModel.getAge());
         Assertions.assertEquals(model.getBreed(), responseModel.getBreed());
@@ -27,7 +32,27 @@ public class CatCreationTest extends CatServiceTest {
         Assertions.assertEquals(model.getTemperament(), responseModel.getTemperament());
         Assertions.assertEquals(model.getStaffInCharge(), responseModel.getStaffInCharge());
         Assertions.assertEquals(model.getIsAdopted(), responseModel.getIsAdopted());
-        // Assertions.assertEquals(model.getAdopterId(), responseModel.getAdopterId());
     }
 
+    @Test
+    @Tag("Smoke")
+    @DisplayName("Crear un nuevo gato, retorna data esperada, solo casos malos")
+    void createNewCatFalse() {
+        CatModel model = defaultCat();
+        ResponseContainer<CatResponse> response = service.addCat(model, null);
+        Assertions.assertEquals(201, response.getStatus());
+        CatResponse responseModel = response.getData();
+
+        Assertions.assertNotNull(response.getData());
+
+        Assertions.assertNotEquals("Peludo", responseModel.getName());
+        Assertions.assertNotEquals(5, responseModel.getAge());
+        Assertions.assertNotEquals("Ragdoll", responseModel.getBreed());
+        Assertions.assertNotEquals("2024-07-09T15:05:23.414Z", responseModel.getDateJoined());
+        Assertions.assertNotEquals(false, responseModel.getVaccinated());
+        Assertions.assertNotEquals(Arrays.asList("Aggressive"), responseModel.getTemperament());
+        Assertions.assertNotEquals("00000000-0000-0000", responseModel.getStaffInCharge());
+        Assertions.assertNotEquals(true, responseModel.getIsAdopted());
+        Assertions.assertNotEquals(model.getAdopterId(), responseModel.getAdopterId());
+    }
 }
